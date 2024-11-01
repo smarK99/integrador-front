603cceadd9bf62020832b9e208cd2144df8df375
smarK99 <santikb99@gmail>
2024-11-01 09:23:33 -0300

    Modal}

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 import { renderCategories } from "./src/services/categories";
+import "./style.css";
 
 renderCategories();
--- src/services/categories.js
+++ src/services/categories.js
@@ -2,8 +2,10 @@
 
 export const renderCategories = () =>{
     
+    //Tomamos la lista ul
     const ulList = document.getElementById("list-filter");
 
+    //Creamos elementos dentro de esta lista
     ulList.innerHTML = `
     <li id="todo"> Todos los productos </li>
     <li id="hamburguesas"> Hamburguesas </li>
@@ -12,10 +14,29 @@ export const renderCategories = () =>{
     <li id="mayor-precio"> Mayor precio </li>
     <li id="menor-precio"> Menor precio </li>
     `;
+
+    //Iteramos los items de la lista de arriba
+    //Reconocemos x consola cada elemento clickeado
+    //Llamamos a una funcion que le pone estilos al elemento clickeado
     const lielements = ulList.querySelectorAll("li");
     lielements.forEach( e => {
         e.addEventListener('click', () => {
-        console.log("click en ", e.id)
+            console.log("click en ", e.id)
+            handleClick(e);
         });
     });
+
+    //Verificamos el elemento activo o clickeado
+    //Para agregarle una clase con estilos
+    const handleClick = eclicked => {
+        lielements.forEach(e => {
+            if (e.classList.contains("li-active")) {
+                e.classList.remove("li-active");
+            }else{
+                if(eclicked === e){
+                    e.classList.add("li-active");
+                }
+            }
+        });
+    };
 }
